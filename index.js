d7f629e76d736714e933f067c2ad35606dc061d1
Ivainge <ivainge@gmail.com>
2023-09-18 14:34:32 +0300

    Add chatId list in json file

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 require('dotenv').config();
-const TGApi = require('node-telegram-bot-api')
-const bot = new TGApi(process.env.BOT_TOKEN, {polling: true})
+const fs = require('fs');
+const TGApi = require('node-telegram-bot-api');
+const bot = new TGApi(process.env.BOT_TOKEN, {polling: true});
+
+let myArray = []
+
+let storagejsonraw = fs.readFileSync('./storage.json');
+let storagejson = JSON.parse(storagejsonraw);
+myArray = storagejson;
+
+console.log(`Всего добавленных юзеров: ${myArray.length}, ${myArray}`);
 
 bot.on('message', async msg => {
-    let text = msg.text;
+    const text = msg.text;
     const chatId = msg.chat.id;
     const username = msg.chat.username;
     let startbotflag = 0; 
+    let storagejsonraw = fs.readFileSync('./storage.json');
+    let storagejson = JSON.parse(storagejsonraw);
+    myArray = storagejson;
 
     if(text === '/start') {
         await bot.sendMessage(985471998, `${username} Запустил бота`);
         startbotflag = 1;
     }
-    if (startbotflag === 0) {
-        await bot.sendMessage(985471998, `${username}: "${text}"`);
+
+    // if (startbotflag === 0) {
+    //     await bot.sendMessage(985471998, `${username}: "${text}"`);
+    // }
+
+    let flag = 0;
+    for(var i=0; i<myArray.length; i++) {
+        if(chatId == myArray[i]) {
+            flag = 1;
+        }
     }
-    console.log(msg);
-})
+
+    if(flag == 0) {
+        myArray.push(chatId);
+        fs.writeFile('./storage.json', JSON.stringify(myArray),
+            function (err) {
+                if (err) {
+                    console.error('Crap happens');
+                }
+            }
+        );
+        console.log(`Новый пользователь добавлен в бд с chatId ${chatId}`)
+    } else {
+        bot.sendMessage(chatId, 'Вы уже добавленны в бд')
+    }
+
+    console.log(`Всего добавленных юзеров: ${myArray.length}, ${myArray}`);
+});
